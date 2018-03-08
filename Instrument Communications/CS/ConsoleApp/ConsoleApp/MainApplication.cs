dcf24e93d273b2ae18a7a21c0c56681e3240d885
Jeff Scherrer <jeff@marqmetrix.com>
2018-03-07 17:19:35 -0800

    Updated with latest client api changes and documentation.

--- Instrument Communications/CS/ConsoleApp/ConsoleApp/MainApplication.cs
+++ Instrument Communications/CS/ConsoleApp/ConsoleApp/MainApplication.cs
@@ -3,10 +3,9 @@ using MarqMetrix.Communications.Client;
 using MarqMetrix.Communications.Client.Direct;
 using MarqMetrix.Communications.Instruments;
 using MarqMetrix.Communications.Samples;
+using MarqMetrix.Communications.Security;
 using MarqMetrix.Data.Samples;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp
@@ -16,25 +15,36 @@ namespace ConsoleApp
 
         public static async Task MainAsync()
         {
+            string host = "192.168.90.105";
+            int port = 80;
+            bool useHttps = false;
+            string apiKey;
+            // Request a shortcode to be generated, then get the primary API key using the shortcode.
+            using (DirectClientConnection connection = await DirectClientConnection.RequestShortCodeAsync(host, port, useHttps))
+            {
+                Console.Write("Enter the shortcode provided by the instrument: ");
+                apiKey = await connection.GetApiKeyAsync(Console.ReadLine(), ApiKeyType.Primary);
+            }
+
             // Create the client context
             using (IClientContext clientContext = ClientContext.Factory.CreateDirectClientContext(
                 new DirectOptions
                 {
                     UseHttps = false,
-                    HostName = "192.168.90.105",
+                    HostName = host,
                     Port = 80,
-                    ApiKey = "zLQVNaCkNgrAm66j0+zVLoKYW602xdhcawgIdPm2HcY="
+                    ApiKey = apiKey
                 }))
             {
                 // Open the connection to the instrument
                 await clientContext.OpenAsync();
 
-                // List all of the instruments connected to the gateway instrument
+                // List all of the instruments connected to the gateway instrument, individual instruments should only return one item in the results.
                 ICollectionResult<IInstrumentInfo> instrumentsResult;
                 string instrumentId = null;
                 do
                 {
-                    instrumentsResult = await clientContext.GetInstrumentsAsync(null);
+                    instrumentsResult = await clientContext.GetInstrumentsAsync();
                     foreach (IInstrumentInfo instrumentInfo in instrumentsResult.Items)
                     {
                         Console.WriteLine($"Instrument: {instrumentInfo.Id}");
@@ -43,10 +53,25 @@ namespace ConsoleApp
                     }
                 } while (instrumentsResult.HasMoreItems);
 
+                // Setup dark acquisition options for 5 samples, each at 1 second capture integration times, and average the 5 results into one sample.
+                ISampleAcquisitionOptions darkSampleOptions = new SampleAcquisitionOptions
+                {
+                    IntegrationTime = TimeSpan.FromSeconds(1),
+                    SampleAverageCount = 5,
+                    LaserPower = 0 // 0 - No laser - Dark sample
+                };
+
+                // Acquire the sample. This function will block asynchronously until the sample has been acquired.
+                ISampleInfo darkSampleInfo = await clientContext.AcquireSampleAsync(instrumentId, darkSampleOptions);
+
+                // Retrieve the sample data.
+                ISampleSet darkSampleSet = await clientContext.GetSampleAsync(instrumentId, darkSampleInfo.Id);
+
+
+
                 // Setup acquisition options for 5 samples, each at 1 second capture integration times, and average the 5 results into one sample.
                 ISampleAcquisitionOptions options = new SampleAcquisitionOptions
                 {
-                    DarkSample = DarkSampleUsage.None,
                     IntegrationTime = TimeSpan.FromSeconds(1),
                     SampleAverageCount = 5,
                     LaserPower = 200
--- Instrument Communications/CS/README.md
+++ Instrument Communications/CS/README.md
@@ -10,6 +10,7 @@ You may need to include pre-releases depending on which versions are available, 
 
 ### Using Statements
 Add the following using statements to your code:
+
 ```C#
 using MarqMetrix.Communications;
 using MarqMetrix.Communications.Client;
@@ -17,6 +18,7 @@ using MarqMetrix.Communications.Client.Direct;
 using MarqMetrix.Communications.Instruments;
 using MarqMetrix.Communications.Samples;
 using MarqMetrix.Data.Samples;
+using MarqMetrix.Communications.Security;
 ```
 
 ### Creating the IClientContext
@@ -29,6 +31,17 @@ IClientContext clientContext = ClientContext.Factory.CreateDirectClientContext(
 	    Port = 80,
 	    ApiKey = "zLQVNaCkNgrAm66j0+zVLoKYW602xdhcawgIdPm2HcY="
 	});
+```
+
+### Using a Shortcode to Connect to an Instrument
+Either of the API keys can be retrieved
+
+```C#
+using (DirectClientConnection connection = await DirectClientConnection.RequestShortCodeAsync(host, port, useHttps))
+{    
+    Console.Write("Enter the shortcode provided by the instrument: ");
+    apiKey = await connection.GetApiKeyAsync(Console.ReadLine(), ApiKeyType.Primary);
+}
 ```
 
 ### Listing all Instruments
@@ -46,7 +59,6 @@ do
 ```C#
 ISampleAcquisitionOptions options = new SampleAcquisitionOptions
 {
-    DarkSample = DarkSampleUsage.None,
     IntegrationTime = TimeSpan.FromSeconds(1),
     SampleAverageCount = 5,
     LaserPower = 200
@@ -57,3 +69,20 @@ ISampleInfo sampleInfo = await clientContext.AcquireSampleAsync(instrumentId, op
 ISampleSet sampleSet = await clientContext.GetSampleAsync(instrumentId, sampleInfo.Id);
 ```
 
+### Acquiring a Dark Sample
+Acquiring a dark sample is the same as acquiring a light sample. The only difference is the laser power is set to 0.
+
+```C#
+ISampleAcquisitionOptions darkSampleOptions = new SampleAcquisitionOptions
+{
+    IntegrationTime = TimeSpan.FromSeconds(1),
+    SampleAverageCount = 5,
+    LaserPower = 0 // 0 - No laser - Dark sample
+};
+
+// Acquire the sample. This function will block asynchronously until the sample has been acquired.
+ISampleInfo darkSampleInfo = await clientContext.AcquireSampleAsync(instrumentId, darkSampleOptions);
+
+// Retrieve the sample data.
+ISampleSet darkSampleSet = await clientContext.GetSampleAsync(instrumentId, darkSampleInfo.Id);
+```
